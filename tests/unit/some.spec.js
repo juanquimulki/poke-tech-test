9e194ed8165227711ac88c31e80450a3755abd7e
Juanqui Mulki <juanquimulki@hotmail.com>
2021-09-28 13:56:02 -0300

    Another tests

--- tests/unit/some.spec.js
+++ tests/unit/some.spec.js
@@ -1,12 +1,21 @@
 import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
 import PokeInfo from '@/components/PokeInfo.vue'
+import PokeTable from '@/components/PokeTable.vue'
 
 describe('App', () => {
   // Inspect the raw component options
   it('App has data', () => {
     expect(typeof App.data).toBe('function')
   })
+
+  // it("Request should show error message with certain high ID", () => {
+  //   const wrapper = shallowMount(App);
+  //   let id = "1000";
+  //   expect(wrapper.vm.snackbar).toBe(false); // snackbar is hidden
+  //   wrapper.vm.getInfo(id);  // calling component method
+  //   expect(wrapper.vm.snackbar).toBe(true); // result should be the snackbar
+  // });  
 })
 
 describe("PokeInfo.vue", () => {
@@ -22,5 +31,16 @@ describe("PokeInfo.vue", () => {
       }
     });
     expect(wrapper.find('[data-testid="pokemon-title"]').text()).toBe("Info: " + msg);
+  });
+});
+
+describe("PokeTable.vue", () => {
+  const wrapper = shallowMount(PokeTable);
+
+  it("Method should return an ID number", () => {
+    let url = "https://pokeapi.co/api/v2/pokemon/10";
+    let result = parseInt(wrapper.vm.getIdFromUrl(url));  // calling component method
+    expect(typeof result).toBe('number'); // result should be a number
+    expect(result).toBe(10); // result should be = 10
   });
 });
